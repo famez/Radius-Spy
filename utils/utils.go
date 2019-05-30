a3a3c3c1fdd53c450956893e0f95ea47cd900e11
famez <f.amez1992@gmail.com>
2019-05-30 13:59:13 +0200

    Added CopyMap() method

--- utils/utils.go
+++ utils/utils.go
@@ -20,3 +20,17 @@ func Min(a int, b int) int {
 
 	return b
 }
+
+func CopyMap(m map[string]interface{}) map[string]interface{} {
+	cp := make(map[string]interface{})
+	for k, v := range m {
+		vm, ok := v.(map[string]interface{})
+		if ok {
+			cp[k] = CopyMap(vm)
+		} else {
+			cp[k] = v
+		}
+	}
+
+	return cp
+}
