b4655e189d22e7892f9cc3044333cf5431609d55
famez <f.amez1992@gmail.com>
2019-05-30 13:56:55 +0200

    Added RecalculateMsgAuth method

--- radius/radiuscrypto.go
+++ radius/radiuscrypto.go
@@ -1,8 +1,11 @@
 package radius
 
-import "crypto/md5"
+import (
+	"crypto/hmac"
+	"crypto/md5"
+)
 
-func CalculateResponseAuth(response RadiusPacket, requestAuth [16]byte, secret string) (bool, [16]byte) {
+func CalculateResponseAuth(response *RadiusPacket, requestAuth [16]byte, secret string) (bool, [16]byte) {
 
 	var retVal [16]byte
 
@@ -17,5 +20,43 @@ func CalculateResponseAuth(response RadiusPacket, requestAuth [16]byte, secret s
 	chunk = append(chunk, []byte(secret)...)
 
 	return true, md5.Sum(chunk)
+
+}
+
+func RecalculateMsgAuth(packet *RadiusPacket, reqAuth [16]byte, secret string) bool {
+
+	var msgAuth [16]byte
+
+	for index := range msgAuth {
+		msgAuth[index] = 0
+	}
+
+	packet.SetMessageAuthenticator(msgAuth)
+
+	encoded, chunk := packet.Encode()
+
+	if !encoded {
+		return false
+	}
+
+	if packet.GetCode() != AccessRequest { //If request packet, there is no need to overwrite autheticator field
+		copy(chunk[4:], reqAuth[:]) //Overwrite with reqAuth
+	}
+
+	mac := hmac.New(md5.New, []byte(secret))
+
+	mac.Write(chunk)
+
+	result := mac.Sum(nil)
+
+	if len(result) != 16 {
+		return false
+	}
+
+	copy(msgAuth[:], result)
+
+	packet.SetMessageAuthenticator(msgAuth)
+
+	return true
 
 }
