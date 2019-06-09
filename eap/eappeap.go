40eb3758284dfa7a236fe532d306cc8ee05abc6f
famez <f.amez1992@gmail.com>
2019-06-09 14:04:33 +0200

    Encode method is implemented

--- eap/eappeap.go
+++ eap/eappeap.go
@@ -1,10 +1,8 @@
 package eap
 
 import (
-	"crypto/tls"
 	"encoding/binary"
 	"fmt"
-	"net"
 )
 
 type PeapFlags struct {
@@ -21,17 +19,11 @@ type EapPeap struct {
 	tlsPayload []byte
 }
 
-//EapTLSSession is a loopback tunnel to be able to send the data generated
-//by TLS protocol into the EAP message instead of sending it directly to the network
-type EapTLSSession struct {
-	client net.TCPConn
-	server tls.Conn
-}
-
 func NewEapPeap() *EapPeap {
 
 	header := HeaderEap{
 		msgType: Peap,
+		length:  6,
 	}
 
 	peap := &EapPeap{
@@ -43,12 +35,61 @@ func NewEapPeap() *EapPeap {
 }
 
 func (packet *EapPeap) Encode() (bool, []byte) {
-	return packet.header.Encode()
+
+	lenBytes := 0
+
+	if packet.flags.length {
+		lenBytes = 4
+	}
+
+	payloadLen := 0
+
+	if packet.tlsPayload != nil {
+		payloadLen = len(packet.tlsPayload)
+	}
+
+	packet.header.setLength(uint16(5 /*header*/ + 1 /*Flags*/ + lenBytes + payloadLen))
+
+	//Encode header
+	ok, buff := packet.header.Encode()
+
+	if !ok {
+		return false, nil
+	}
+
+	if packet.GetCode() != EAPRequest && packet.GetCode() != EAPResponse {
+		return false, nil
+	}
+
+	//Encode flags
+	buff[5] = 0
+
+	if packet.flags.length {
+		buff[5] |= 0x80
+	}
+	if packet.flags.moreFrag {
+		buff[5] |= 0x40
+	}
+	if packet.flags.start {
+		buff[5] |= 0x20
+	}
+	buff[5] |= (packet.flags.version & 0x07)
+
+	if packet.flags.length {
+
+		//Encode TLS length
+		binary.BigEndian.PutUint32(buff[6:], packet.tlsLength)
+
+	}
+
+	//Encode Raw TLS data
+	copy(buff[6+lenBytes:], packet.tlsPayload)
+
+	return true, buff
+
 }
 
 func (packet *EapPeap) Decode(buff []byte) bool {
-
-	fmt.Println("func (packet *EapPeap) Decode")
 
 	ok := packet.header.Decode(buff)
 
@@ -62,25 +103,28 @@ func (packet *EapPeap) Decode(buff []byte) bool {
 	packet.flags.start = (buff[5]&0x20 == 0x20)
 	packet.flags.version = (buff[5] & 0x07)
 
+	offset := 6
+
 	if packet.flags.length {
 
 		//Decode TLS length
 		packet.tlsLength = binary.BigEndian.Uint32(buff[6:])
-
-		//Decode Raw TLS data
-		payloadLength := packet.header.GetLength() - 10 //Substract the header data length
-
-		if len(buff[10:]) != int(payloadLength) {
-			fmt.Println("Length mismatch")
-
-			return false
-		}
-
-		packet.tlsPayload = make([]byte, payloadLength)
-
-		copy(packet.tlsPayload, buff[10:])
+		offset += 4
 
 	}
+
+	//Decode Raw TLS data
+	payloadLength := packet.header.GetLength() - uint16(offset) //Substract the header data length
+
+	if len(buff[offset:]) != int(payloadLength) {
+		fmt.Println("Length mismatch")
+
+		return false
+	}
+
+	packet.tlsPayload = make([]byte, payloadLength)
+
+	copy(packet.tlsPayload, buff[offset:])
 
 	return true
 
@@ -116,4 +160,62 @@ func (packet *EapPeap) GetStartFlag() bool {
 
 func (packet *EapPeap) GetVersionFlag() byte {
 	return packet.flags.version
+}
+
+func (packet *EapPeap) GetTLSTotalLength() uint32 {
+	return packet.tlsLength
+}
+
+func (packet *EapPeap) SetId(id uint8) {
+	packet.header.SetId(id)
+}
+
+func (packet *EapPeap) SetCode(code EapCode) {
+	packet.header.SetCode(code)
+}
+
+func (packet *EapPeap) SetTLSPayload(payload []byte) {
+	packet.tlsPayload = append(packet.tlsPayload, payload...)
+
+	lenBytes := 0
+
+	if packet.flags.length {
+		lenBytes = 4
+	}
+
+	payloadLen := len(packet.tlsPayload)
+
+	length := 5 /*header*/ + 1 /*Flags*/ + lenBytes + payloadLen
+
+	packet.header.setLength(uint16(length))
+
+}
+
+func (packet *EapPeap) SetLengthFlag(lengthFlag bool) {
+
+	if lengthFlag != packet.flags.length {
+		if lengthFlag {
+			packet.header.setLength(packet.header.GetLength() + 4)
+		} else {
+			packet.header.setLength(packet.header.GetLength() - 4)
+		}
+	}
+
+	packet.flags.length = lengthFlag
+}
+
+func (packet *EapPeap) SetMoreFlag(more bool) {
+	packet.flags.moreFrag = more
+}
+
+func (packet *EapPeap) SetStartFlag(start bool) {
+	packet.flags.start = start
+}
+
+func (packet *EapPeap) SetVersionFlag(version byte) {
+	packet.flags.version = version
+}
+
+func (packet *EapPeap) SetTLSTotalLength(length uint32) {
+	packet.tlsLength = length
 }
