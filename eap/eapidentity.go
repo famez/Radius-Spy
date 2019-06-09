b2368cb79bfd3e8d95e69e3c4fdcf19814e40e2d
famez <f.amez1992@gmail.com>
2019-06-09 13:58:51 +0200

    Added eapidentity packet

---
+++ eap/eapidentity.go
@@ -0,0 +1,54 @@
+package eap
+
+type EapIdentity struct {
+	header   HeaderEap
+	identity string
+}
+
+func NewEapIdentity() *EapIdentity {
+
+	header := HeaderEap{
+		msgType: Identity,
+	}
+
+	identity := &EapIdentity{
+		header: header,
+	}
+
+	return identity
+
+}
+
+func (packet *EapIdentity) Encode() (bool, []byte) {
+	return packet.header.Encode()
+}
+
+func (packet *EapIdentity) Decode(buff []byte) bool {
+
+	ok := packet.header.Decode(buff)
+
+	if !ok {
+		return false
+	}
+
+	packet.identity = string(buff[5:])
+
+	return true
+
+}
+
+func (packet *EapIdentity) GetId() uint8 {
+	return packet.header.GetId()
+}
+
+func (packet *EapIdentity) GetCode() EapCode {
+	return packet.header.GetCode()
+}
+
+func (packet *EapIdentity) GetType() EapType {
+	return packet.header.GetType()
+}
+
+func (packet *EapIdentity) GetIdentity() string {
+	return packet.identity
+}
