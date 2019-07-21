b3dfa40a1c43164a7251be0896858a9361a1f4ab
famez <f.amez1992@gmail.com>
2019-07-21 16:44:35 +0200

    Added functions:
    msChapV2CryptoHashNtPasswordHash
    MsChapV2GenerateAuthenticatorResponse

--- eap/eapCrypto.go
+++ eap/eapCrypto.go
@@ -3,16 +3,29 @@ package eap
 import (
 	"crypto/des"
 	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"io"
+	"strings"
 
 	"golang.org/x/crypto/md4"
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
 )
 
+var magic1 = [...]byte{0x4D, 0x61, 0x67, 0x69, 0x63, 0x20, 0x73, 0x65, 0x72, 0x76,
+	0x65, 0x72, 0x20, 0x74, 0x6F, 0x20, 0x63, 0x6C, 0x69, 0x65,
+	0x6E, 0x74, 0x20, 0x73, 0x69, 0x67, 0x6E, 0x69, 0x6E, 0x67,
+	0x20, 0x63, 0x6F, 0x6E, 0x73, 0x74, 0x61, 0x6E, 0x74}
+
+var magic2 = [...]byte{0x50, 0x61, 0x64, 0x20, 0x74, 0x6F, 0x20, 0x6D, 0x61, 0x6B,
+	0x65, 0x20, 0x69, 0x74, 0x20, 0x64, 0x6F, 0x20, 0x6D, 0x6F,
+	0x72, 0x65, 0x20, 0x74, 0x68, 0x61, 0x6E, 0x20, 0x6F, 0x6E,
+	0x65, 0x20, 0x69, 0x74, 0x65, 0x72, 0x61, 0x74, 0x69, 0x6F,
+	0x6E}
+
 //rfc2759 8.2
-func msChapV2CryptoChallengeHash(peerChallenge [16]byte, authChallenge [16]byte, username string) []byte {
+func msChapV2CryptoChallengeHash(peerChallenge, authChallenge [16]byte, username string) []byte {
 
 	h := sha1.New()
 
@@ -43,8 +56,18 @@ func msChapV2CryptoNtPasswordHash(password string) []byte {
 
 }
 
+//rfc2759 8.4
+func msChapV2CryptoHashNtPasswordHash(passwordHash []byte) []byte {
+
+	h := md4.New()
+	h.Write(passwordHash)
+
+	return h.Sum(nil)
+
+}
+
 //rfc2759 8.5
-func msChapV2CryptoChallengeResponse(challenge []byte, psswdHash []byte) []byte {
+func msChapV2CryptoChallengeResponse(challenge, psswdHash []byte) []byte {
 
 	var psswdHashZ [21]byte
 
@@ -119,7 +142,7 @@ func msChapV2CryptoChallengeResponse(challenge []byte, psswdHash []byte) []byte 
 }
 
 //rfc2759 8.1
-func MsChapV2GenerateNTResponse(authChallenge [16]byte, peerChallenge [16]byte, username string, password string) []byte {
+func MsChapV2GenerateNTResponse(authChallenge, peerChallenge [16]byte, username string, password string) []byte {
 
 	challenge := msChapV2CryptoChallengeHash(peerChallenge, authChallenge, username)
 
@@ -130,4 +153,33 @@ func MsChapV2GenerateNTResponse(authChallenge [16]byte, peerChallenge [16]byte, 
 		return response
 	}
 	return nil
+}
+
+//rfc2759 8.7
+func MsChapV2GenerateAuthenticatorResponse(password string, ntResponse [24]byte, peerChallenge, authChallenge [16]byte, username string) string {
+
+	psswdHash := msChapV2CryptoNtPasswordHash(password)
+	psswdHashHash := msChapV2CryptoHashNtPasswordHash(psswdHash)
+
+	hsha1 := sha1.New()
+
+	hsha1.Write(psswdHashHash)
+	hsha1.Write(ntResponse[:])
+	hsha1.Write(magic1[:])
+
+	digest := hsha1.Sum(nil)
+	challenge := msChapV2CryptoChallengeHash(peerChallenge, authChallenge, username)
+
+	hsha1 = sha1.New()
+
+	hsha1.Write(digest)
+	hsha1.Write(challenge)
+	hsha1.Write(magic2[:])
+
+	digest = hsha1.Sum(nil)
+
+	message := hex.EncodeToString(digest)
+	message = "S=" + strings.ToUpper(message)
+
+	return message
 }
