617053d3920bde1fc96e39298b22a97942da06e5
famez <f.amez1992@gmail.com>
2019-07-28 19:48:13 +0200

    Added keyring material to context

--- session/tlsSession.go
+++ session/tlsSession.go
@@ -3,6 +3,7 @@ package session
 import (
 	"bufio"
 	"crypto/tls"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net"
@@ -20,7 +21,12 @@ type TLSSession struct {
 	nasSideTunnel    TLSLocalTunnel
 	serverSideTunnel TLSLocalTunnel
 
+	nasTLSVersion uint16 //TLS version of the NAS side TLS connection
+
 	tlsClientAvailable sync.WaitGroup
+
+	//To derive keys from TLS session data. This data is very sensitive
+	keyring KeyringData
 
 	//To track the number of handshake packets that have been processed, so that we know if the 4-way handshake process has finished or not
 	nasHandshakeStat uint
@@ -28,10 +34,26 @@ type TLSSession struct {
 	serverHandshakeStat uint
 }
 
+//KeyringData to store sensitive data from TLS session
+type KeyringData struct {
+	randomClient    [32]byte
+	randomServer    [32]byte
+	masterSecret    [48]byte
+	masterSecretSet bool
+}
+
+type keyRingChannelWriter struct {
+	keyRingChannels map[[32]byte]chan [48]byte
+}
+
 var tlsLocalListener net.Listener
 var tcpLocalListener net.Listener
 
+var keyringWriter *keyRingChannelWriter //To keep track of the master secret used to derive WIFI session keys (when using PEAP)
+
 func initLocalTLSServer() {
+
+	keyringWriter = newKeyRingChannelWriter()
 
 	cer, err := tls.LoadX509KeyPair("TestKeys/server.crt", "TestKeys/server.key")
 
@@ -42,6 +64,7 @@ func initLocalTLSServer() {
 			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 			tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
 		},
+		KeyLogWriter: keyringWriter,
 	}
 	tlsLocalListener, err = tls.Listen("tcp", "localhost:400", config)
 	if err != nil {
@@ -64,6 +87,8 @@ func NewTLSSession() *TLSSession {
 
 	tlsSession.nasHandshakeStat = 0
 	tlsSession.serverHandshakeStat = 0
+
+	tlsSession.keyring.masterSecretSet = false
 
 	//To know whether we have access to the TLS client or not as it is being initialized asynchronously
 	tlsSession.tlsClientAvailable.Add(1)
@@ -242,4 +267,133 @@ func (session TLSSession) GetServerHandShakeStatus() uint {
 
 func (session *TLSSession) SetServerHandShakeStatus(status uint) {
 	session.serverHandshakeStat = status
+}
+
+func (session TLSSession) GetRandomClient() [32]byte {
+	return session.keyring.randomClient
+}
+
+func (session *TLSSession) SetRandomClient(random []byte) {
+
+	if len(random) != 32 {
+		return
+	}
+	copy(session.keyring.randomClient[:], random)
+}
+
+func (session TLSSession) GetRandomServer() [32]byte {
+	return session.keyring.randomServer
+}
+
+func (session *TLSSession) SetRandomServer(random []byte) {
+
+	if len(random) != 32 {
+		return
+	}
+	copy(session.keyring.randomServer[:], random)
+
+}
+
+func (session *TLSSession) GetMasterSecret() [48]byte {
+
+	if !session.keyring.masterSecretSet {
+		var ok bool
+		if ok, session.keyring.masterSecret = keyringWriter.getMasterSecret(session.keyring.randomClient); ok {
+			session.keyring.masterSecretSet = true
+		}
+	}
+
+	return session.keyring.masterSecret
+}
+
+func (session TLSSession) GetNASVersion() uint16 {
+	return session.nasTLSVersion
+}
+
+func (session *TLSSession) SetNASVersion(version uint16) {
+	session.nasTLSVersion = version
+}
+
+//keyRingChannelWriter functions and methods
+
+func newKeyRingChannelWriter() *keyRingChannelWriter {
+
+	writer := &keyRingChannelWriter{}
+	writer.keyRingChannels = make(map[[32]byte]chan [48]byte)
+	return writer
+}
+
+func (writer *keyRingChannelWriter) Write(p []byte) (n int, err error) {
+
+	line := string(p)
+
+	var randomClientStr, masterSecretStr string
+
+	fmt.Sscanf(line, "CLIENT_RANDOM %s %s\n", &randomClientStr, &masterSecretStr)
+
+	decoded, err := hex.DecodeString(randomClientStr)
+
+	if err != nil {
+		return 0, err
+	}
+
+	var randomClient [32]byte
+
+	copy(randomClient[:], decoded)
+
+	decoded, err = hex.DecodeString(masterSecretStr)
+
+	if err != nil {
+		return 0, err
+	}
+
+	var masterSecret [48]byte
+
+	copy(masterSecret[:], decoded)
+
+	//Generate channel if it does not exist
+	if writer.keyRingChannels[randomClient] == nil {
+		writer.keyRingChannels[randomClient] = make(chan [48]byte, 1) //Buffer only one array to be read later in another routine
+	}
+
+	//Reference to the channel
+	channel := writer.keyRingChannels[randomClient]
+
+	//Not blocking channel
+	select {
+	case channel <- masterSecret:
+	default:
+	}
+
+	return len(p), nil
+}
+
+func (writer *keyRingChannelWriter) getMasterSecret(randomClient [32]byte) (bool, [48]byte) {
+
+	var retVal [48]byte
+
+	//Check channel
+	if writer.keyRingChannels[randomClient] == nil {
+		return false, retVal
+	}
+
+	channel := writer.keyRingChannels[randomClient]
+
+	//Not blocking channel
+	select {
+	case retVal = <-channel:
+	default:
+		return false, retVal
+	}
+
+	//Get the value only once
+	writer.clearChannel(randomClient)
+
+	return true, retVal
+}
+
+func (writer *keyRingChannelWriter) clearChannel(randomClient [32]byte) {
+
+	writer.keyRingChannels[randomClient] = nil
+
 }
